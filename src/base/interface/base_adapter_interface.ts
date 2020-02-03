0287155abab06394a12113ee796913b788863901
Eric Mendes Dantas <ericdantas0@gmail.com>
2020-02-03 14:48:35 -0300

    Improves the way the interfaces are separated

--- src/base/interface/base_adapter_interface.ts
+++ src/base/interface/base_adapter_interface.ts
@@ -1,13 +1,16 @@
-export interface IBaseAdapter<DTO, ENTITY> {
-    convertFromMuven(entity:ENTITY, additionalParams?: any): Promise<DTO>;
+export interface IConversionToMuven<DTO, ENTITY> {
     convertToMuven(dto:DTO, additionalParams?: any): Promise<ENTITY>;
 }
 
-export interface IBaseValidatorAdapter<DTO, ENTITY> {
+export interface IConversionFromMuven<DTO, ENTITY> {
+    convertFromMuven(entity:ENTITY, additionalParams?: any): Promise<DTO>;
+}
+
+export interface IAdapterValidation<DTO, ENTITY> {
     validateBeforeSync(entity:ENTITY): void;
     validateAfterSync(dto:DTO): void;
 }
 
-export interface IBaseNewComparator<DTO> {
+export interface IAdapterNewComparator<DTO> {
     isNew(dto: DTO): boolean;
 }
